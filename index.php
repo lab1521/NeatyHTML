2d59c2bcfbd8cc3a84ac92773c6f0e1211baf025
Lab1521 <marcz@lab1521.com>
2016-07-31 03:29:27 +1200

    Example file

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require('vendor/autoload.php');
 use Lab1521\NeatyHTML;
 
-//This will remove onerror attribute does not executing eval to alert
+//Removes onerror attribute which prevents eval to alert
 $html = '<img src=x:alert(window) onerror=eval(src) alt=0>';
 $neaty = new NeatyHTML($html);
 //Outputs <img src="x:alert(window)" alt="0">
