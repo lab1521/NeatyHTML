fd63caea7ebbb0f9f4a393f6c19f5cc3bdd9f539
Lab1521 <marcz@lab1521.com>
2016-08-03 08:05:35 +1200

    More example in index.php

--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 require('vendor/autoload.php');
 use Lab1521\NeatyHTML;
 
-//Removes onerror attribute which prevents eval to alert
-$html = '<img src=x:alert(window) onerror=eval(src) alt=0>';
-$neaty = new NeatyHTML($html);
-//Outputs <img src="x:alert(window)" alt="0">
+//Goal: Remove onerror attribute which prevents eval to alert
+$badImage = '<img src=x:alert(window) onerror=eval(src) alt="bad image">';
+$goodImage = '<img src="images/good.gif" alt="good image">';
+
+$neaty = new NeatyHTML($badImage . $goodImage);
+
+//Outputs <img src="x:alert(window)" alt="bad image"><img src="images/good.gif" alt="good image">
+echo $neaty->tidyUp()->html();
+
+//Further restrictions with source images
+$neaty->blockedTags(['img']);
+$neaty->tagOverrides([
+    'img' => [
+        [
+            'attribute' => 'src',
+            'values' => ['images/'] //restricts to local folder
+        ],
+    ]
+]);
+
+//Goal: Remove $badImage
+$neaty->loadHtml($badImage . $goodImage);
+
+//Outputs $goodImage only
 echo $neaty->tidyUp()->html();
